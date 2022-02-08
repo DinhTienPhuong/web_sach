04a430b006a8d1948a6d71cbfffd3361365c89a9
DinhTienPhuong <phuongdinh3100@gmail.com>
2022-02-08 18:05:42 +0700

    update

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+// Edited by ThietkewebX.net
+
+// Cấu hình kết nối dữ liệu
+$hostname     = "localhost"; 
+$username     = "root"; 
+$password     = ""; 
+$databasename = "web_sach";
+
+// Cấu hình hệ thống
+$visitorTimeout = 900; //=15 * 60
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+// Cấu hình ngôn ngữ.
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+);
+
+?>
+
